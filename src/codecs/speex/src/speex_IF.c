8859a079fa6fc9969a07348fd477407f389e5820
animant <animant@email.post>
2015-06-06 22:41:10 +0300

    Add samples for speex encode/decode

---
+++ src/codecs/speex/src/speex_IF.c
@@ -0,0 +1,61 @@
+#include <speex/speex.h>
+#include <stdio.h>
+
+/*The frame size in hardcoded for this sample code but it doesn't have to be*/
+#define FRAME_SIZE 160
+
+void *state;
+SpeexBits bits;
+char cbits[200];
+int nbBytes;
+short in[FRAME_SIZE];
+float input[FRAME_SIZE];
+
+int encoder_initialize()
+{
+   /*Holds the state of the encoder*/
+   /*Holds bits so they can be read and written to by the Speex routines*/
+   SpeexBits bits;
+   int i, tmp;
+   /*Create a new encoder state in narrowband mode*/
+   state = speex_encoder_init(&speex_nb_mode);
+   /*Set the quality to 8 (15 kbps)*/
+   tmp=8;
+   speex_encoder_ctl(state, SPEEX_SET_QUALITY, &tmp);
+   /*Initialization of the structure that holds the bits*/
+   speex_bits_init(&bits);
+}
+
+int encoder_finalize()
+{
+  speex_encoder_destroy(state);
+  /*Destroy the bit-packing struct*/
+  speex_bits_destroy(&bits);
+}
+
+int encode_buffer(void *dst, void *src, int size)
+{
+    //TODO: eliminate file, add buffer working
+   int i;
+   while (1)
+   {
+     /*Read a 16 bits/sample audio frame*/
+     fread(in, sizeof(short), FRAME_SIZE, fin);
+     if (feof(fin))
+        break;
+     /*Copy the 16 bits values to float so Speex can work on them*/
+     for (i=0;i<FRAME_SIZE;i++)
+        input[i]=in[i];
+     /*Flush all the bits in the struct so we can encode a new frame*/
+     speex_bits_reset(&bits);
+     /*Encode the frame*/
+     speex_encode(state, input, &bits);
+     /*Copy the bits to an array of char that can be written*/
+     nbBytes = speex_bits_write(&bits, cbits, 200);
+     /*Write the size of the frame first. This is what sampledec expects but
+      it’s likely to be different in your own application*/
+     fwrite(&nbBytes, sizeof(int), 1, stdout);
+     /*Write the compressed data*/
+     fwrite(cbits, 1, nbBytes, stdout);
+  }
+}
